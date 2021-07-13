fa98394e3fa6c9b92afa4386dd910855b934a692
Jr-Lij <jr_lij@cnceo.club>
2021-07-13 14:21:58 +0800

    数组类型判断

---
+++ src/arr/TypeCheck.php
@@ -0,0 +1,58 @@
+<?php
+namespace lij\arr;
+
+class TypeCheck{
+    /**
+     * 数组类型判断
+    */
+    public static function is_dict($arr){
+        //return array_keys($arr) !== range(0,count($arr)-1);
+        if(!is_array($arr)) return false;
+        $ak = array_keys($arr);
+        foreach($ak as $k){
+            if(is_numeric($k)) return false;
+        }
+        return true;
+    }
+    public static function is_list($arr){
+        if(!is_array($arr)) return false;
+        return array_keys($arr) === range(0,count($arr)-1);
+    }
+    public static function is_mix_arr($arr){
+        if(!is_array($arr)) return false;
+        return self::is_dict($arr)||self::is_list($arr)?false:true;
+    }
+    /**
+     * 一维字典
+    */
+    public static function is_odarr($arr){
+        if(!self::is_dict($arr)) return false;
+        foreach ($arr as $v){
+            if(!is_numeric($v)&&!is_string($v)&&!is_bool($v)&&!is_null($v)) return false;
+        }
+        return true;
+    }
+    /**
+     * 一维字典组成的数组
+    */
+    public static function is_odlistbydict($arr){
+        if(!self::is_list($arr)) return false;
+        foreach ($arr as $v){
+            if(!self::is_odarr($v)) return false;
+        }
+        return true;
+    }
+    /**
+     * 二维列表
+    */
+    public static function is_tdlist($arr){
+        if(!self::is_list($arr)) return false;
+        foreach ($arr as $v){
+            if(!self::is_list($v)) return false;
+        }
+        return true;
+    }
+}
+
+
+?>
