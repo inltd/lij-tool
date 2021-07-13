2a8080312c7886a66475ba8f56d97cc0eb922273
Jr-Lij <jr_lij@cnceo.club>
2021-07-13 14:36:52 +0800

    增加字符串转换和验证

---
+++ src/str/StringConversion.php
@@ -0,0 +1,19 @@
+<?php
+namespace lij\str;
+
+/**
+ * 字符串转换
+*/
+class StringConversion{
+    public static function to_safe_query($str){
+        return preg_match('/^.+$/',$str)?preg_replace('/[^a-zA-Z]/','',$str):false;
+    }
+    public static function only_chinese($str){
+        return preg_match('/^.+$/',$str)?preg_replace('/[^\u4e00-\u9fa5]/','',$str):false;
+    }
+    public static function to_safe_filename($str){
+        return preg_match('/^.+$/',$str)?preg_replace('/[^-a-zA-Z0-9\.\p{Han}]/u','_',$str):false;
+    }
+}
+
+?>
---
+++ src/str/StringVerification.php
@@ -0,0 +1,26 @@
+<?php
+namespace lij\str;
+
+/**
+ * 字符串校验
+*/
+class StringVerification{
+
+    public static function is_safe_query($str){
+        return preg_match('/^[a-zA-Z]+$/i', $str)?true:false;
+    }
+
+    public static function is_chinese($str){
+        return preg_match('/^[\u4e00-\u9fa5]+$/', $str)?true:false;
+    }
+
+    public static function is_safe_filename($str){
+        return preg_match('/^[-a-zA-Z0-9\.\p{Han}_]+$/u',$str)?true:false;
+    }
+
+    public static function is_jdbh($str){
+        return preg_match('/^[a-zA-Z][-a-zA-Z0-9]{9,17}$/i',$str)?true:false;
+    }
+}
+
+?>
